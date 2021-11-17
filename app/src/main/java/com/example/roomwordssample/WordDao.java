ab7c5a1532aef243772366752d042d2c5f543813
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-11-17 17:23:35 +0100

    Add XML layouts for the UIPopulate the database

--- app/src/main/java/com/example/roomwordssample/WordDao.java
+++ app/src/main/java/com/example/roomwordssample/WordDao.java
@@ -1,12 +1,14 @@
 package com.example.roomwordssample;
 
 import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
 
+@Dao
 public interface WordDao {
     @Insert
     void insert(Word word);
--- app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -8,6 +8,7 @@ import androidx.room.DatabaseConfiguration;
 import androidx.room.InvalidationTracker;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
@@ -26,6 +27,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                             // if no Migration object.
                             // Migration is not part of this practical.
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
@@ -51,4 +53,14 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     public void clearAllTables() {
 
     }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
 }
