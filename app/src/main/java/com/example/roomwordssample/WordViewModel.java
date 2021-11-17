7f9f896aabef61bffac1e8f043a77ade9bf04ee1
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-11-17 22:36:37 +0100

    Delete all words

--- app/src/main/java/com/example/roomwordssample/WordViewModel.java
+++ app/src/main/java/com/example/roomwordssample/WordViewModel.java
@@ -30,4 +30,7 @@ public class WordViewModel extends AndroidViewModel {
     LiveData<List<Word>> getAllWords() { return mAllWords; }
 
     public void insert(Word word) { mRepository.insert(word); }
+
+    public void deleteAll() {mRepository.deleteAll();}
+
 }
